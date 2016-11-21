301ba5c8abd4e99620051acc21261930acd7e694
Nacho Gonzalez <nachomieres@gmail.com>
2016-11-21 11:24:52 +0100

    Añadido plataforma android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
+//angular fire para firebase
+import { AngularFireModule } from 'angularfire2';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSIJ5QyOh90Q92wQG2C4BCnZp8mmtECCo",
+  authDomain: "dondeando.firebaseapp.com",
+  databaseURL: "https://dondeando.firebaseio.com",
+  storageBucket: "project-6272698264410432201.appspot.com",
+  messagingSenderId: "300387384343"
+};
 
 @NgModule({
   declarations: [
@@ -9,7 +20,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
