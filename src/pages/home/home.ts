89295713e2d60e6ad4832b5237f8ab60f43a21f1
Nacho Gonzalez <nachomieres@gmail.com>
2016-11-23 12:43:12 +0100

    ultimo de hoy

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,12 +26,12 @@ export class HomePage {
       // Configuracion del BackgroundGeolocation
       let config = {
         desiredAccuracy: 10,
-        stationaryRadius: 20,
-        distanceFilter: 30,
+        stationaryRadius: 100,
+        distanceFilter: 5,
         notificationTitle: 'Dondevoy',
         notificationText: 'Guardando ruta...',
         debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+        stopOnTerminate: true, // enable this to clear background location settings when the app terminates
       };
       BackgroundGeolocation.configure((location) => {
         // cada vez que obtiene una posicion, la guarda en la base de datos
@@ -43,13 +43,13 @@ export class HomePage {
       }, (error) => {
         alert ('BackgroundGeolocation error');
       }, config); // configuracion del BackgroundGeolocation
-
+      console.log ('buscando posicion...');
       // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
       Geolocation.getCurrentPosition().then(pos => {
         console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
         //BackgroundGeolocation.start();
         this.permisoBackground = true;
-      }); // Captura la posicion y una vez la consigue empieza a captuar en segundo plano
+      }); 
     }); // platform ready
   } // constructor
 
@@ -63,6 +63,15 @@ export class HomePage {
     this.authData.logoutUser().then(() => {
     this.navCtrl.setRoot(LoginPage);
   });
+  }
+  onInit () {
+    console.log ('buscando posicion...');
+    // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+    Geolocation.getCurrentPosition().then(pos => {
+      console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+      //BackgroundGeolocation.start();
+      this.permisoBackground = true;
+    }); // Captura la posicion y una vez la consigue empieza a captuar en segundo plano
   }
 
 } // Class home
